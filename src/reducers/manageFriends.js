ec4520373d2a4ee76013a84c54eca9b872a99aa2
James <jamescscalise@gmail.com>
2020-05-25 21:10:38 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+    switch (action.type){
+        case 'ADD_FRIEND':
+            return {friends: 
+                [
+                state.friends[0],
+                action.friend
+                ]
+                
+            }
+        case 'REMOVE_FRIEND':
+            let newArray = state.friends.filter(function( obj ) {
+                return obj.id !== action.id;
+              });
+            return {friends:
+                newArray
+            }
+        default:
+            return state 
+    }
 }
